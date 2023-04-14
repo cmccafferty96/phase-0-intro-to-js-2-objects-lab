bbe618e4aa34ba074af18e1f5f157110e4e23b05
+ <+>
2023-04-14 11:23:34 -0400

    completed phase0 intro to js objects lab

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
-const employee = {}
-employee.name=Sam
-employee.streetAddress=Broadway
-function updateEmployeeWithKeyAndValue(employee, key, value) {
-    const newEmployee = {...employee}
-    newEmployee[key]= value
-    return newEmployee
+const employee = {
+    name: 'Sam',
+    streetAddress: 'Broadway',
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee;
 }
 
-function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
-    employee[key]= value
-    return employee
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
 }
 
-function deleteFromEmployeeByKey(employee, key) {
-    const newEmployee={...employee}
-    delete newEmployee[key]
-    return newEmployee
+function deleteFromEmployeeByKey(employee, key){
+    const newEmployee = {...employee};
+    delete newEmployee[key];
+    return newEmployee;
 }
 
-function destructivelyDeleteFromEmployeeByKey(employee, key) {
-    delete employee[key]
-    return employee
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
 }
